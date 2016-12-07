6fa9d699a9909047c5fe474d86436c8e1f69cf6b
Raul Migdonio Rodriguez Te <raulmigdonio@gmail.com>
2016-12-07 08:15:13 -0600

    Ahora la actividad de todas las palabras, muestra en tarjetas la informacion recibida junto con un evento onclick para cada una

--- app/src/main/java/migdonio1/sharedjapanese/app/WordsListActivity.java
+++ app/src/main/java/migdonio1/sharedjapanese/app/WordsListActivity.java
@@ -1,14 +1,18 @@
 package migdonio1.sharedjapanese.app;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
 
 import migdonio1.sharedjapanese.R;
+import migdonio1.sharedjapanese.app.fragments.CardsListRecyclerViewAdapter;
 import migdonio1.sharedjapanese.endpoints.WordsEndpointInterface;
 import migdonio1.sharedjapanese.models.Word;
 import retrofit2.Call;
@@ -21,13 +25,23 @@ import static migdonio1.sharedjapanese.data.APIConstants.API_ENDPOINT;
 
 public class WordsListActivity extends AppCompatActivity {
 
-    private TextView holis;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private static String LOG = "WordsListActivity";
+
+    private List<Word> words;
+
     private WordsEndpointInterface apiWords;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_words_list);
+
+        mRecyclerView = (RecyclerView)findViewById(R.id.my_recycler_view);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(API_ENDPOINT)
@@ -39,15 +53,31 @@ public class WordsListActivity extends AppCompatActivity {
         getWords();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
     public void getWords(){
         Call<List<Word>> call = apiWords.WordsList();
         call.enqueue(new Callback<List<Word>>() {
             @Override
             public void onResponse(Call<List<Word>> call, Response<List<Word>> response) {
-                List<Word> words = response.body();
+                words = response.body();
+                mAdapter = new CardsListRecyclerViewAdapter(words);
+                mRecyclerView.setAdapter(mAdapter);
+
+                ((CardsListRecyclerViewAdapter) mAdapter)
+                        .setOnItemClickListener(new CardsListRecyclerViewAdapter.CardClickListener(){
+
+                            @Override
+                            public void onItemClick(int position, View view) {
+                                Log.d(LOG, "Clicked card " + position);
+                            }
+                        });
 
                 for(int i=0; i<words.size(); i++){
-                    Log.d("WordsListActivity", "Word: " + words.get(i).getOriginal() + " (" + words.get(i).getSyllables() + ")");
+                    Log.d(LOG, "Word: " + words.get(i).getOriginal() + " (" + words.get(i).getSyllables() + ")");
                 }
             }
 
---
+++ app/src/main/java/migdonio1/sharedjapanese/app/fragments/CardsListRecyclerViewAdapter.java
@@ -0,0 +1,89 @@
+package migdonio1.sharedjapanese.app.fragments;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+import migdonio1.sharedjapanese.R;
+import migdonio1.sharedjapanese.models.Word;
+
+/**
+ * Created by migdonio1 on 12/7/16.
+ */
+public class CardsListRecyclerViewAdapter extends RecyclerView.Adapter<CardsListRecyclerViewAdapter.WordHolder> {
+
+    private static String LOG = "CardsListRecyclerViewAdapter";
+    private List<Word> mWordset;
+    private static CardClickListener cardClickListener;
+
+    @Override
+    public WordHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_view_row, parent, false);
+        WordHolder dataWordHolder = new WordHolder(view);
+
+        return dataWordHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(WordHolder holder, int position) {
+        holder.original.setText(mWordset.get(position).getOriginal());
+        holder.syllables.setText(mWordset.get(position).getSyllables());
+        holder.translates.setText(mWordset.get(position).getTranslates().get(0));
+        holder.types.setText(mWordset.get(position).getTypes().get(0));
+    }
+
+    public void addItem(Word word, int index) {
+        mWordset.add(index, word);
+        notifyItemInserted(index);
+    }
+
+    public void deleteItem(int index) {
+        mWordset.remove(index);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mWordset.size();
+    }
+
+    public static class WordHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView original;
+        TextView syllables;
+        TextView translates;
+        TextView types;
+
+        public WordHolder (View itemView){
+            super(itemView);
+            original = (TextView) itemView.findViewById(R.id.card_view_original_text);
+            syllables = (TextView) itemView.findViewById(R.id.card_view_syllables_text);
+            translates = (TextView) itemView.findViewById(R.id.card_view_translates_text);
+            types = (TextView) itemView.findViewById(R.id.card_view_types_text);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            cardClickListener.onItemClick(getAdapterPosition(), view);
+        }
+    }
+
+    public void setOnItemClickListener(CardClickListener cardClickListener){
+        this.cardClickListener = cardClickListener;
+    }
+
+    public CardsListRecyclerViewAdapter(List<Word> mWordset){
+        this.mWordset = mWordset;
+    }
+
+    public interface CardClickListener {
+        public void onItemClick(int position, View view);
+    }
+}
